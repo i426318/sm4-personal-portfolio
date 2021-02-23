b20ebd9627d7f9646e285e16f93f1c1c2caf3967
i426318 <w.biantoro@student.fontys.nl>
2021-02-23 20:10:33 +0100

    updated GURU APP

--- android-dev/GuruAppLogin2/app/src/main/java/com/example/guruapplogin/MainActivity.kt
+++ android-dev/GuruAppLogin2/app/src/main/java/com/example/guruapplogin/MainActivity.kt
@@ -2,25 +2,49 @@ package com.example.guruapplogin
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val button: Button = findViewById(R.id.button)
+        var username: EditText = findViewById(R.id.editTextUsername)
+        var password: EditText = findViewById(R.id.editTextPassword)
+
         button.setOnClickListener { // when button is clicked
-            Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show()
+
+            checkDataEntered(username, password)
+
         }
     }
-
-    private fun readName() {
-        // reads whatever name the user inputs at the username, and password
+    private fun checkDataEntered(a: EditText, b: EditText) {
+        /*
+        Reads whatever name the user inputs at the username, and password
         // assuming user has signed in beforehand.
         // if username matches credentials + password is correct,
         // welcomes user to a new page, that says "Hello, [username]"
         // if not, Toast em!
+
+        Future implementations:
+        - Data for username and password.
+        - Registration page.
+        - Checking password etc.
+        - Actually making it work.
+         */
+
+        if (a === null || b === null) { // username is gone, password is gone. DOESN'T WORK YET ;-;
+            Toast.makeText(this, "Incomplete information!", Toast.LENGTH_SHORT).show()
+        }
+        else { // task complete.
+            var usernameVal = a.getText().toString() //gets text and converts to string.
+            Toast.makeText(this, "Welcome, $usernameVal!", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
 }
