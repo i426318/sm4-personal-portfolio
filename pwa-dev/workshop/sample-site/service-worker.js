886db46290ecdf55893921a23f5f9f4fb38bca10
i426318 <i426318@fhict.nl>
2021-04-26 11:31:43 +0700

    updated pwa site with push notifications permissions and added new data files for subscriptions.

---
+++ pwa-dev/workshop/sample-site/service-worker.js
@@ -0,0 +1,79 @@
+
+// 1.1 - Caching Important PWA files for offline use.
+// beating heart of a pwa.
+
+var cacheName = "pwaCache";
+var appFiles = [
+  "/manifest.json",
+  "/scripts.js",
+  "/stylesheet.css",
+  "/index.html",
+  "/logo.png",
+  "/yes.gif",
+  "/service-worker.js"
+];
+
+
+
+// install phase. One-time.
+self.addEventListener("install", e => {
+  console.log("Service Worker: I am being installed, hello world!");
+
+  // 1.1 - Cache the files via install event.
+  e.waitUntil( // taking a promise.
+
+    // opening a cache in our dir.
+    // then, we have the 'cache' Object.
+    caches.open(cacheName).then(cache => {
+      console.log("Service Worker: Caching important offline files");
+      // passing through list of assets.
+      return cache.addAll(appFiles);
+    }).catch(e => {
+      console.log("caching failed with error msg: " + e);
+    })
+  );
+
+});
+
+
+self.addEventListener("activate", (activating) => {
+  console.log("Service Worker: All systems online, ready to go!");
+});
+
+
+// fetch listener. This will run once we have a 'fetch' event.
+
+self.addEventListener("fetch", e => {
+  console.log('Intercepting fetch req for: ${e.request.url}');
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  )
+
+});
+
+
+
+self.addEventListener("push", (pushing) => {
+  console.log("lets do some push push...");
+
+  if(pushing.data){
+    pushdata=JSON.parse(pushing.data.text());		
+    console.log("Service Worker: I received this:",pushdata);
+
+    if((pushdata["title"]!="")&&(pushdata["message"]!="")){			
+      const options={ body:pushdata["message"] }
+      self.registration.showNotification(pushdata["title"],options);
+      console.log("Service Worker: I made a notification for the user");
+    } else {
+      console.log("Service Worker: I didn't make a notification for the user, not all the info was there :(");			
+    }
+  }
+})
+
+self.addEventListener("notificationclick",function(clicking){
+  const pageToOpen="/scripts.js";
+	const promiseChain=clients.openWindow(pageToOpen);
+	event.waitUntil(promiseChain);
+});
---
+++ pwa-dev/workshop/w3-push-notif-sender/index.js
@@ -0,0 +1,77 @@
+//Setup libraries we want to use (installed them first with *npm install*)
+const webpush = require("web-push");
+const fetch = require("node-fetch");
+const prompts = require("prompts");
+
+//Location of your subscribers file (normally they would be in a database)
+const yourSubscriberJSONFileURL =
+  "https://www.i426318.hera.fhict.nl/subscribers.json";
+
+//Place your keys here
+webpush.setVapidDetails(
+  "mailto:w.biantoro@student.fontys.nl", //Subject VAPID
+  "BIU_a4odFqF7yRn01o6j0IeMTu9CF5IwyXBjPTed5yiy5RLn_2Ncc-hg-tzeioviyzmGGqR9DxfEk8lFIDdQ0q4", // Public Key VAPID
+  "uNbzK9Su9tw-Q_9pXrWoSL7qdD56KgSZ496N6oHIgKA" //Private Key VAPID
+);
+
+console.log(
+  " ==========================================\n",
+  "==       SEND PUSH NOTIFICATION         ==\n",
+  "==========================================\n"
+);
+
+//This allows you to fill in the titel and message of the notification in the CLI
+const questions = [
+  {
+    type: "text",
+    name: "title",
+    message: "Push notification title"
+  },
+  {
+    type: "text",
+    name: "message",
+    message: "Push notification message"
+  }
+];
+
+(async () => {
+  const answers = await prompts(questions);
+
+  console.log(
+    "\n ==========================================\n",
+    "==    SENDING MESSAGE TO SUBSCRIBERS    ==\n",
+    "==========================================\n"
+  );
+
+  //Putting the promted title and message in variables to use
+  let pushTitle = answers.title;
+  let pushMessage = answers.message;
+
+  //Send a notification to every subscriber
+  fetch(yourSubscriberJSONFileURL)
+    .then(subscriberJSON => subscriberJSON.json())
+    .then(subscriberJSON => {
+      for (let subscriberEndpoint in subscriberJSON) {
+        //Setting up format of subcription for sending
+        const pushSubscription = {
+          endpoint: subscriberEndpoint,
+          keys: {
+            auth: subscriberJSON[subscriberEndpoint]["keys"]["auth"],
+            p256dh: subscriberJSON[subscriberEndpoint]["keys"]["p256dh"]
+          }
+        };
+        //Actual sending
+        webpush
+          .sendNotification(
+            pushSubscription,
+            `{"title":"${pushTitle}","message":"${pushMessage}"}`
+          )
+          .then(result => {
+            console.log(`-- Message send to ${pushSubscription.endpoint}`);
+          })
+          .catch(error => {
+            console.log(`-- Message NOT send to ${pushSubscription.endpoint}`);
+          });
+      }
+    });
+})();
